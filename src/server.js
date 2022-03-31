0fd6afc19d7901d6908bdb220eb8c659df942272
haeeema@naver.com <83750390+haeeema@users.noreply.github.com>
2022-03-31 23:56:55 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-app.listen(4500);
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const hadleListen = () =>
+  console.log(`✅ Listening on http://localhost:4500 🔥`);
+app.listen(4500, hadleListen);
