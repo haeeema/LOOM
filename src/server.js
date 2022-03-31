133a328f103ac98dca0683ba4586f3d77bd0d6dc
haeeema@naver.com <83750390+haeeema@users.noreply.github.com>
2022-04-01 00:11:18 +0900

    catchall url

--- src/server.js
+++ src/server.js
@@ -6,8 +6,10 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
-
+// This line is what exposes the public folder to users.
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+// ⭐️Catchall URL
 
 const hadleListen = () =>
   console.log(`✅ Listening on http://localhost:4500 🔥`);
