f7ce7de08bcb819fdf60ad5d65bd00634a0d1dac
haeeema@naver.com <83750390+haeeema@users.noreply.github.com>
2022-04-06 16:54:21 +0900

    #3 reset

--- src/server.js
+++ src/server.js
@@ -7,101 +7,13 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-
 app.use("/public", express.static(__dirname + "/public"));
-// This line is what exposes the public folder to users.
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-// NEW!! Catchall URL
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
-// We have to install socket io in the backend and in the frontend.
-instrument(wsServer, {
-  auth: false,
-});
+const wsServer = new Server(httpServer);
 
-function publicRooms() {
-  const sids = wsServer.sockets.adapter.sids;
-  const rooms = wsServer.sockets.adapter.rooms;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anonymous";
-
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-  });
-
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-/* 
-const wss = new WebSocketServer({ server });
-// Create WebSocket server on http sever.
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous";
-  console.log("✅ Connected to Browser");
-  socket.on("close", () => console.log("⛔️ Disconnected from Browser"));
-  socket.on("message", (message) => {
-    const parsedMessage = JSON.parse(message);
-    switch (parsedMessage.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${parsedMessage.payload}`)
-        );
-        break;
-      case "nickname":
-        socket["nickname"] = parsedMessage.payload;
-        break;
-      // 🔥 Put payload inside our socket. socket is basically object.
-    }
-  });
-  // Method of socket, not method of ws.
-});
-// On method is going to give us some information about the person that just connectied to our backend.
-// On server.js, <socket> represents the browser that just connected. */
-
-const hadleListen = () =>
-  console.log(`✅ Listening on http://localhost:4500 🍋`);
+const hadleListen = () => console.log(`Listening on http://localhost:4500`);
 
 httpServer.listen(4500, hadleListen);
-// only http => app.listen(4500, handleListen);
